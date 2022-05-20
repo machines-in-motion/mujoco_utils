8fdc2ceb09a893312ce2f167e433102633a92318
Julian Viereck <julian.viereck@gmail.com>
2022-05-20 15:02:36 -0400

    Fix MujocoWrapper.get_end_effector_forces: Return forces in world frame

--- src/mujoco_utils/__init__.py
+++ src/mujoco_utils/__init__.py
@@ -6,7 +6,6 @@ All rights reserved.
 """
 
 import numpy as np
-
 from dm_control import mujoco
 
 
@@ -17,7 +16,7 @@ class MujocoWrapper:
             mujoco_endeff_geom_id: The mujoco id of the endeffector to check contact with.
         """
         self.physics = physics
-        
+
         self.nq = pinocchio_robot.nq
         self.nv = pinocchio_robot.nv
         self.nj = len(joint_names)
@@ -28,52 +27,52 @@ class MujocoWrapper:
 
         self.joint_names = joint_names
         self.mujoco_endeff_geom_id = np.array(mujoco_endeff_geom_id)
-    
+
     def step_simulation(self):
         self.physics.step()
-        
+
     def get_state(self):
         # TODO: Should loop over the joint_names.
         # TODO: Need to make it work for fixed-base robots.
         q = np.array(self.physics.named.data.qpos)
         dq = np.array(self.physics.named.data.qvel)
-        
+
         # TODO: Need to do the quaternion check general.
         # Need to swap quaternion into pinocchio convention.
         quat = q[3:7].copy()
         q[3:6] = quat[1:]
         q[6] = quat[0]
-        
+
         return q, dq
-        
+
     def send_joint_command(self, tau):
         # TODO: Do proper mapping.
         self.physics.data.ctrl = tau
-        
+
     def reset_state(self, q, dq):
         q_mjco = q.copy()
         q_mjco[3] = q[6]
         q_mjco[4] = q[3]
         q_mjco[5] = q[4]
         q_mjco[6] = q[5]
-        
+
         with self.physics.reset_context():
             self.physics.data.qpos = q_mjco
             self.physics.data.qvel = dq
-            
+
     def get_end_effector_forces(self):
         return self.end_effector_forces()
-            
+
     def end_effector_forces(self):
         contact = self.physics.data.contact
         cnt_geom1 = contact.geom1
         cnt_geom2 = contact.geom2
-        
+
         cnt_status = np.zeros(self.nf)
         cnt_forces = np.zeros((self.nf, 6))
-        
+
         eff_id = None
-        
+
         for i in range(len(cnt_geom2)):
             if cnt_geom2[i] in self.mujoco_endeff_geom_id:
                 eff_id = np.where(self.mujoco_endeff_geom_id == cnt_geom2[i])[0][0]
@@ -83,9 +82,16 @@ class MujocoWrapper:
                 continue
 
             cnt_status[eff_id] = 1
-            cnt_forces[eff_id] = self.physics.data.contact_force(i).reshape(-1)
+            force, tau = self.physics.data.contact_force(i)
 
-            # Setting small torques and forces to zero.
-            cnt_forces[eff_id][np.where(np.abs(cnt_forces[eff_id]) < 1e-4)[0]] = 0
-            
+            # Rotate the force and torque into the world frame.
+            rot = contact.frame[i].reshape(3, 3)
+            force = force @ rot
+            tau = tau @ rot
+
+            # Setting small torques to zero.
+            tau[np.where(np.abs(tau) < 1e-4)[0]] = 0.
+
+            cnt_forces[i] = np.hstack([force, tau])
+
         return cnt_status, cnt_forces
